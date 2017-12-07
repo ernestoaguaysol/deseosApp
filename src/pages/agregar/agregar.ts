7804cfea5ceed0d67e752c5620e92021852b6296
Ernesto <ernestoaguaysol@gmail.com>
2017-12-07 16:16:04 -0300

    index clases, update variables.scss md button none. Funtion agregar and borrar in component agregar

--- src/pages/agregar/agregar.ts
+++ src/pages/agregar/agregar.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { ListaItem } from '../../clases/index';
 
 @Component({
   selector: 'page-agregar',
@@ -8,13 +9,32 @@ import { NavController, NavParams } from 'ionic-angular';
 export class AgregarPage {
 
   nombreLista:string;
-  nombreItem:string;
+  nombreItem:string = "";
+
+  items:ListaItem[] = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AgregarPage');
+  }
+
+  agregar(){
+    if (this.nombreItem.length == 0) {
+      return;
+    }
+    let item = new ListaItem();
+    item.nombre = this.nombreItem;
+
+    this.items.push( item );
+    this.nombreItem = "";
+  }
+
+  borrarItem(index: number){
+    console.log('presiona el boton borrar');
+    
+    this.items.splice(index, 1);
   }
 
 }
